97dd263a9579b26e5cec374af00c846b20a5269c
noa-lif <noalif65@gmail.com>
2024-11-19 22:38:04 +0200

    HW-2

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,22 @@
 // Demonstrates the Collatz conjecture.
+
 public class Collatz {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
+
+    public static void main(String args[]) {
+        int number = Integer.parseInt(args[0]);
+        int counter = 1;
+
+        System.out.print(number + " ");
+        while (number != 1 || counter == 1) {
+            if (number % 2 == 0) {
+                number = number / 2;
+            } else {
+                number = number * 3;
+                number++;
+            }
+            System.out.print(number + " ");
+            counter++;
+        }
+        System.out.print("(" + counter + ")");
+    }
 }
